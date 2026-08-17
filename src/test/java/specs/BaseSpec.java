f4b4c18211703402c51fb0afdc5a408a63e911d4
Артем Избышев <izbyshev-aa@mail.ru>
2026-08-18 02:35:07 +0300

    Добавлен allure

--- src/test/java/specs/BaseSpec.java
+++ src/test/java/specs/BaseSpec.java
@@ -1,8 +1,10 @@
 package specs;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 
+import static allure.CustomAllureListener.withCustomTemplate;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.ALL;
 import static io.restassured.http.ContentType.JSON;
@@ -10,6 +12,7 @@ import static io.restassured.http.ContentType.JSON;
 public class BaseSpec {
 
     public static RequestSpecification requestSpec = with()
+            .filter(withCustomTemplate())
             .log().all()
             .contentType(JSON)
             .basePath("api/v1");
